f9053430a534532ceacdaabe005857c519b40b2f
achmand- Dylan Vassallo <dylan.vassallo@hotmail.com>
2017-03-14 16:05:46 +0100

    Initial Commit

---
+++ XMLRPCServerPythonCSharp/Client/IIsEven.cs
@@ -0,0 +1,11 @@
+﻿using CookComputing.XmlRpc;
+
+namespace Client
+{
+    [XmlRpcUrl("http://localhost:8000/RPC2")]
+    public interface IIsEven : IXmlRpcProxy
+    {
+        [XmlRpcMethod("IsEven")]
+        bool IsEven(int n);
+    }
+}
---
+++ XMLRPCServerPythonCSharp/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CookComputing.XmlRpc;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxy = XmlRpcProxyGen.Create<IIsEven>();
+            var x = proxy.IsEven(4);
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+    }
+}
